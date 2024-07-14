1bb8f8b19ae6bb4f6e9217ac654aa05cd99dc87d
rockydeveloper001 <139945062+rockydeveloper001@users.noreply.github.com>
2024-07-14 13:20:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const img = document.createElement('img');
         img.className = 'card-img-top';
-        img.src = 'images/movie-placeholder.jpg'; // Path to your actual image
+        img.src = 'https://archive.org/embed/20240714-131541'; // Path to your actual image
         img.alt = movie.title;
 
         const cardBody = document.createElement('div');
